b34d1cf920f13b0ec5de93810a9bec1a8ee0b903
John <john@triplingo.com>
2012-11-07 20:37:26 +0000

    added template for extension

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
-Spree::Core::Engine.routes.draw do
+Spree::Core::Engine.routes.prepend do
   # Add your extension routes here
+  namespace :api do
+    scope :module => :v1 do
+      namespace :users do
+        post   :coupon_add
+        post   :coupon_remove
+      end
+    end
+  end
 end
